579ce25a663fede0e01851242ad66bc0594bb029
vasile <58362597+singularity-bit@users.noreply.github.com>
2021-05-13 13:43:25 +0300

    Revert "Back end"

--- Controllers/PUT_REQUESTS.js
+++ Controllers/PUT_REQUESTS.js
@@ -1,24 +1,11 @@
 
 const changeUser=(req,res,db)=>{
     const {id,user_type}=req.body;
+    
     if(user_type==='pacient'){
-        const user={
-            'nume_pacient':req.body.nume_pacient,
-            'prenume_pacient':req.body.prenume_pacient,
-            'cnp_pacient':req.body.cnp_pacient,
-            'tel_nr':req.body.tel_nr,
-            'email':req.body.email
-        }
-        db('pacienti').where({id}).update(user).then(result=>res.json(result)).catch(err=>res.json(err));
+        db('pacienti').where({id}).update(req.body).then(result=>res.json(result)).catch(err=>res.json(err));
     }else if(user_type==='medic'){
-        const user={
-            'nume_medic':req.body.nume_medic,
-            'prenume_medic':req.body.prenume_medic,
-            'cnp_medic':req.body.cnp_medic,
-            'tel_nr':req.body.tel_nr,
-            'email':req.body.email
-        }
-        db('medici').where({id}).update(user).then(result=>res.json(result)).catch(err=>res.json(err));
+        db('medici').where({id}).update(req.body).then(result=>res.json(result)).catch(err=>res.json(err));
     }
     
 }
