75a43cab96d62827f6140b7b2cff5e123daec94c
singularity-bit <maxdsl98@gmail.com>
2021-05-15 15:43:23 +0300

    fixed delete

--- Controllers/DELETE_REQUESTS.js
+++ Controllers/DELETE_REQUESTS.js
@@ -2,9 +2,9 @@
 const deleteUser=(req,res,db)=>{
     const {id,user_type}=req.body;
     if(user_type==='pacient'){
-        db('pacienti').where({id}).del().then(result=>res.json(result)).catch(err=>res.json(err));
+        db('pacienti').where({id:id}).del().then(result=>res.json(result)).catch(err=>res.json(err));
     }else if (user_type==='medic'){
-        db('medici').where({id}).del().then(result=>res.json(result)).catch(err=>res.json(err));
+        db('medici').where({id:id}).del().then(result=>res.json(result)).catch(err=>res.json(err));
     }
 }
 
